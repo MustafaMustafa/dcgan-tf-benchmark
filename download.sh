3f8a37be75c72bda879421bac29528118adfb262
Mustafa Mustafa <mstftsm@gmail.com>
2017-08-24 10:09:43 -0700

    Add 512 and remove 1000 size images

--- download.sh
+++ download.sh
@@ -3,5 +3,6 @@ cd data
 wget http://portal.nersc.gov/project/star/mustafa/dcgan_benchmark_data/benchmark_GAN_generated_cosmo_maps_64.npy
 wget http://portal.nersc.gov/project/star/mustafa/dcgan_benchmark_data/benchmark_GAN_generated_cosmo_maps_128.npy
 wget http://portal.nersc.gov/project/star/mustafa/dcgan_benchmark_data/benchmark_GAN_generated_cosmo_maps_256.npy
-wget http://portal.nersc.gov/project/star/mustafa/dcgan_benchmark_data/benchmark_GAN_generated_cosmo_maps_1000.npy
+wget http://portal.nersc.gov/project/star/mustafa/dcgan_benchmark_data/benchmark_GAN_generated_cosmo_maps_512.npy
+# wget http://portal.nersc.gov/project/star/mustafa/dcgan_benchmark_data/benchmark_GAN_generated_cosmo_maps_1000.npy
 cd ..
